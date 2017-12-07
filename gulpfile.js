1c55385d517165ca04b3891934eff984378e9ff3
Stephen Phillips <stephen.phillips@bigpond.com>
2017-12-07 23:11:06 +1100

    Added watch and default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat'),
+	compass = require('gulp-compass'),
 	browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -9,6 +10,8 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 	];
+var sassSources =  ['components/sass/style.scss'];
+
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -23,3 +26,24 @@ gulp.task('js', function(){
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('css', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			images: 'builds/development/images',
+			style: 'expanded'
+		}))
+			.on('error', gutil.log
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/css')))
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+
+gulp.task('watch', function(){
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+});
+
